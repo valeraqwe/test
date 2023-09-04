6dc7986c461f1e13f1f8a51630633e5455b6e4b8
valeraqwe <valera.komar.1999@gmail.com>
2023-09-04 18:37:28 +0300

    Added Department model with basic CRUD operations

---
+++ models/Department.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once '../config/db.php';
+
+class Department {
+    private Database $db;
+
+    public function __construct() {
+        $this->db = new Database();
+    }
+
+    public function getAll(): false|array
+    {
+        $query = "SELECT * FROM departments";
+        $stmt = $this->db->pdo->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function add($name): bool
+    {
+        $query = "INSERT INTO departments (name) VALUES (:name)";
+        $stmt = $this->db->pdo->prepare($query);
+        $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+        return $stmt->execute();
+    }
+
+    public function delete($id): bool
+    {
+        $query = "DELETE FROM departments WHERE id = :id";
+        $stmt = $this->db->pdo->prepare($query);
+        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+        return $stmt->execute();
+    }
+}
