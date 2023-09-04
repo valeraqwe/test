d408011c3d4ff72d64b6e82040409c62d5d68a9b
valeraqwe <valera.komar.1999@gmail.com>
2023-09-04 18:46:35 +0300

    Added index page and landing view

---
+++ views/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Employee Management System</title>
+</head>
+<body>
+<h1>Welcome to the Employee Management System</h1>
+<nav>
+    <ul>
+        <li><a href="/departments">Manage Departments</a></li>
+        <li><a href="/employees">Manage Employees</a></li>
+    </ul>
+</nav>
+</body>
+</html>
